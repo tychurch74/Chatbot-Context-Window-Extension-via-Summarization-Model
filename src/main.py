5cb34a24203faa26ed284a81490cd4ce132c51ef
Tyler Church <tychurch74@gmail.com>
2023-04-17 15:01:10 -0400

    api_key environment variable

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import openai
+import os
 from modules.text_summarization import Summarizer
 from data.io_utils import write_json, json_obj, add_to_json
 
 # Settings
-openai.api_key = "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"
+openai.api_key = os.environ["OPENAI_API_KEY"]
 enable_json_output = False
 num_iterations = 3
 chunk_size = 4
