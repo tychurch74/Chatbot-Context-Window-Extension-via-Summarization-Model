1457674e368079d5208de368931310b6e793f446
Tyler Church <tychurch74@gmail.com>
2023-04-17 13:50:41 -0400

    main.py refactor and README edits

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# ml_template
-Template repo that includes various functions and tools for ML projects
+# GPT-3.5-turbo Chatbot with Conversation Summarization
+This Python script uses OpenAI's GPT-3.5-turbo language model to create a chatbot that provides short, concise responses based on the context of the conversation. It includes a conversation summarization feature that allows the chatbot to retain a summary of previous interactions and simulate long-term memory, even for lengthy conversations.
+
+## Problem
+Chatbots utilizing GPT-3.5-turbo have a limited token count, restricting the context window size. If the conversation grows too long, the context window will truncate the conversation, causing the chatbot to lose prior interactions' context. By using a conversation summarizer, the chatbot can maintain a summarized context, keeping the token count within acceptable limits and simulating long-term memory.
+
+## How to Use
+1. Install the required dependencies:
+
+```
+pip install openai
+pip install nltk
+pip install transformers
+pip install torch
+pip install pandas
+pip install pillow
+```
+
+2. Navigate to src/main.py and add your OpenAI API key to the openai.api_key setting. Implement a secure method to store and load your API key.
+
+3. Modify the settings at the beginning of the code as required, such as the num_iterations, chunk_size, completion_file_path, and message_history_file_path.
+
+4. Run the script in your terminal.
+
+5. Enter your input when prompted. The chatbot will provide responses based on the context of the summarized conversation.
+
+6. The conversation data will be logged into JSON files specified by the completion_file_path and message_history_file_path settings if enable_json_output is set to True.
+
+
+## Settings
+You can modify the following settings in the script:
+
+openai.api_key: Your OpenAI API key (implement a secure method to load this value).
+
+enable_json_output: A boolean value to enable or disable JSON file outputs (True by default).
+
+num_iterations: The number of user inputs the script will accept in the main loop (3 by default).
+
+chunk_size: The size of chunks for the Summarizer (4 by default).
+
+completion_file_path: The file path for the completion.json file.
+
+message_history_file_path: The file path for the message_history.json file.
+
+
+## Custom modules:
+modules.text_summarization: A custom text summarization module.
+
+data.io_utils: Custom I/O utility functions for JSON files.
--- src/main.py
+++ src/main.py
@@ -1,52 +1,56 @@
 import openai
-import json
-
 from modules.text_summarization import Summarizer
 from data.io_utils import write_json, json_obj, add_to_json
 
+# Settings
 openai.api_key = "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"
-                                
+enable_json_output = False
+num_iterations = 3
+chunk_size = 4
+completion_file_path = "completion.json"
+message_history_file_path = "message_history.json"
+
 message_history = []
 
-def conversation_sum(message_history):
+def generate_conversation_summary(message_history):
     if not message_history:
         conversation_summary = [{"role": "system", "content": "You are a chatbot that gives short, concise responses based on the following context (no conversation history yet)"}]
-        
     else:
         content = [message["content"] for message in message_history]
         combined_content = "".join(content)
-
         summarizer = Summarizer()
-        corrected = summarizer.process_in_chunks(combined_content, chunk_size=4)
+        summary = summarizer.process_in_chunks(combined_content, chunk_size=chunk_size)
+        conversation_summary = [{"role": "system", "content": f"You are a chatbot that gives short, concise responses based on the following context {summary}"}]
 
-        conversation_summary = [{"role": "system", "content": f"You are a chatbot that gives short, concise responses based on the following context {corrected}"}]
+    return conversation_summary
 
-    return(conversation_summary)
-
+def log_to_json(data, file_path):
+    if enable_json_output:
+        add_to_json(data, file_path)
 
 def chat(inp, role="user"):
-    #message_history.append({"role": role, "content": f"{inp}"})
-    conversation_summary = conversation_sum(message_history)
-    conversation_summary.append({"role": role, "content": f"{inp}"})
+    conversation_summary = generate_conversation_summary(message_history)
+    conversation_summary.append({"role": role, "content": inp})
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=conversation_summary
     )
     chat_data = json_obj(conversation_summary, completion)
-    add_to_json(chat_data, "completion.json") 
+    log_to_json(chat_data, completion_file_path)
     reply_content = completion.choices[0].message.content
-    message_history.append({"role": "assistant", "content": f"{reply_content}"})
+    message_history.append({"role": "assistant", "content": reply_content})
     return reply_content
 
+def main():
+    for i in range(num_iterations):
+        user_input = input("> ")
+        print("User's input was:", user_input)
+        print(chat(user_input))
+        print()
 
-for i in range(3):
-    user_input = input("> ")
-    print("User's input was: ", user_input)
-    print(chat(user_input))
-    print()
+    conversation_summary = generate_conversation_summary(message_history)
+    data = json_obj(message_history, conversation_summary)
+    write_json(data, message_history_file_path)
 
-con_sum = conversation_sum(message_history)
-data = json_obj(message_history, con_sum)
-write_json(data, "message_history.json")
-
-
+if __name__ == "__main__":
+    main()
